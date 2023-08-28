0b9febf3e5f8eb0537715ea095a3a21348dee5b8
saaci <saacifuentesmu@unal.edu.co>
2023-08-27 23:03:10 -0500

    - feat(decrement): decrement homework implemented.
    Initialized the variables in main function.
    Added a routine to decerment the value.
    Used an infinite loop to finish the program.
    
    Reference:

--- Src/main.s
+++ Src/main.s
@@ -1,28 +1,30 @@
-    .section .text
-    .syntax unified
-    .thumb
-    .global main
+.section .text
+.syntax unified
+.thumb
+.global main
 
-    @ Define a RAM address as memory_location
-.equ MEM_LOC, 0x20000200
+.equ MEM_LOC, 0x20000200 @ Define a RAM address as MEM_LOC
+
+decrement:
+    ldr r0, [r2]         @ Load the value at address in R2 into R0
+    subs r0, r0, r1      @ Decrease the value in R0 by R1
+    str r0, [r2]         @ Store R0 back into the address in R2
+    cmp r0, #0           @ Compare the new value in R0 with 0
+    ble loop             @ If the value is less than or equal to 0, jump to 'loop'
+    movw r0, #0          @ Clear r0 for next iteration
+    add r3, r3, #1       @ Increase the iteration counter
+    b decrement
 
 main:
-    @ Store 42 in memory
-    movw r2, #42           @ Load 42 into R0
-    movt r0, #42
-    movw r0, #17
-    ldr  r1, =MEM_LOC      @ Load high half-word of memory_location into R1
-    str  r0, [r1]          @ Store R0 into the address in R1
+    ldr r0, =#813017     @ Load id into R0
+    movw r1, #1017       @ Load date into R1 Less Significant Bits (LSB)
 
-    @ Load 42 from memory
-    ldr  r0, [r1]          @ Load the value at address in R1 into R0
+    ldr r2, =MEM_LOC     @ Load memory_location into R2
+    str r0, [r2]         @ Store R0 into the address in R2
+    movw r3, #0          @ initialize R3 to store the iteration counter
 
-    @ Add 7
-    addw r0, r0, #7        @ Add 7 to R0
-
-    @ Store back in memory
-    str  r0, [r1]          @ Store R0 back into the address in R1
+    bl decrement         @ Call the decrement function
 
     @ End (Infinite loop)
 loop:
-    b loop
+    b loop               @ Stay in this loop forever
